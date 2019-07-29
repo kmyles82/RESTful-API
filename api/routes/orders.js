e7aeccbd17ce61cbc5926af2a1ab3b9ceb48d92f
kmyles82 <kmyles82@gmail.com>
2019-07-29 17:42:40 -0500

    added order and products object to that will be parsed

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+//Handle GET request to /orders
 router.get('/', (req, res, next) => {
     res.status(200).json({
         orders: 'Orders were fetched'
     })
 })
 
+//Handle POST request to create an order -> /orders
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        orders: 'Orders was created'
+        orders: 'Orders was created',
+        order: order
     })
 })
 
+//Handle GET request to a single /orders/:id
 router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         orders: 'Orders details',
@@ -20,6 +28,7 @@ router.get('/:orderId', (req, res, next) => {
     })
 })
 
+//Handle DELETE request to a single /orders:id
 router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
         orders: 'Orders deleted',
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/')
+//Handle GET request to /products
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    })
+})
+
+//Handle POST request to /products
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    })
+})
+
+//Handle GET request to a single product at /products:id
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id,
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID',
+        })
+    }
+})
+
+//Handle PATCH request to update a single product at /products:id
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated Product',
+    })
+})
+
+//Handle DELETE request to a single product at /products:id
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted Product',
+    })
+})
+
+module.exports = router;
