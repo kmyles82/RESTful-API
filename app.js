71251d1a52b0bff50dd7bbdc19bdc166316efc7e
kmyles82 <kmyles82@gmail.com>
2019-07-29 18:02:36 -0500

    setup middleware to handle CORS errors

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
+
+    next();
 });
 
 //Routes which should handle requests
