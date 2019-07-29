87e432117100073c200bef20d93be80b030d9459
kmyles82 <kmyles82@gmail.com>
2019-07-29 17:10:25 -0500

    added orders.js route methods

---
+++ api/routes/orders.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        orders: 'Orders were fetched'
+    })
+})
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        orders: 'Orders was created'
+    })
+})
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        orders: 'Orders details',
+        orderId: req.params.orderId
+    })
+})
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        orders: 'Orders deleted',
+        
+    })
+})
+module.exports = router;
