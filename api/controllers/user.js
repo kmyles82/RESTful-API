41c706383f79c782b9b739426670e4cb79274ba0
kmyles82 <kmyles82@gmail.com>
2019-08-01 11:08:10 -0500

    added controllers to the project

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,12 +1,10 @@
-const express = require("express");
-const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
-router.post("/signup", (req, res, next) => {
+exports.user_signup = (req, res, next) => {
   User.find({ email: req.body.email })
     .exec()
     .then(user => {
@@ -44,9 +42,9 @@ router.post("/signup", (req, res, next) => {
         });
       }
     });
-});
+};
 
-router.post("/login", (req, res, next) => {
+exports.user_login = (req, res, next) => {
   User.find({ email: req.body.email })
     .exec()
     .then(user => {
@@ -69,7 +67,7 @@ router.post("/login", (req, res, next) => {
             },
             process.env.JWT_KEY,
             {
-                expiresIn: "1h"
+              expiresIn: "1h"
             }
           );
           return res.status(200).json({
@@ -88,9 +86,9 @@ router.post("/login", (req, res, next) => {
         error: err
       });
     });
-});
+};
 
-router.delete("/:userId", (req, res, next) => {
+exports.user_delete = (req, res, next) => {
   User.remove({ _id: req.params.userId })
     .exec()
     .then(result => {
@@ -104,6 +102,4 @@ router.delete("/:userId", (req, res, next) => {
         error: err
       });
     });
-});
-
-module.exports = router;
+};
